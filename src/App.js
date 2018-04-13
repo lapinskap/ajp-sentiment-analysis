ae0521d4ec9704f4848ccc282a14be03cfee4779
Paulina <cherry3475@gmail.com>
2018-04-13 20:55:30 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import Button from 'react-bootstrap/lib/Button';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+ReactDOM.render(<p>Here is my great new app</p> , document.getElementById('app'));
