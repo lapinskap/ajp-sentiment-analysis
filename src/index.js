99f78149905c57f31fc18b7a4dca7a618e076073
Paulina Łapińska <cherry3475@gmail.com>
2018-07-19 10:45:10 +0200

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import './styles/style.scss';
-import './Bootstrap.css';
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
